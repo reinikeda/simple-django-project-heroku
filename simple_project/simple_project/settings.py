19adc98542d2e4c2af3a75fcc4d7a24918787ac8
reinikeda <reinikeda@gmail.com>
2023-10-27 10:21:29 +0300

    Few changed to work on Heroku

--- simple_project/simple_project/settings.py
+++ simple_project/simple_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-avg^_i@$2a$319o2rcf647%qeu1e2klyd83x&(@9ldp84#-fho'
+# SECRET_KEY = 'django-insecure-avg^_i@$2a$319o2rcf647%qeu1e2klyd83x&(@9ldp84#-fho'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
